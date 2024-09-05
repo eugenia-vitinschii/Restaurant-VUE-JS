c1ec9cd52d73cc18f08c2fd89b9d65877746c086
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2024-09-05 19:23:22 +0300

    add responsive styles, add animation for elements, add responsive style for text elements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,41 @@
 <template>
   <cardboard-section
-    :heading="'lorem'"
-    :subheading="'loremssd'"
-    :button="'dedede'"
+    :heading="'Only Organic'"
+    :subheading="'Order fresh products with delivery'"
+    :button="'view products'"
     :img="'src/assets/img/lime.png'"
     :media="'src/assets/img/color-veg-1.png'"
   />
   <transparent-section
     :class="'photo'"
-    :heading="'lorem'"
-    :subheading="'loremssd'"
-    :button="'dedede'"
+    :heading="'Tasty menu'"
+    :subheading="'Fresh salads, soups and side dishes'"
+    :button="'view menu'"
   />
   <cardboard-section
-    :heading="'lorem'"
-    :subheading="'loremssd'"
-    :button="'dedede'"
+    :heading="'Sliced ​​vitamins'"
+    :subheading="'sliced ​​vegetables and fruits for your holidays from the most delicious vegetables'"
+    :button="'view menu'"
     :img="'src/assets/img/cabbage.png'"
     :media="'src/assets/img/color-veg-2.png'"
   />
   <transparent-section
-    :heading="'lorem'"
-    :subheading="'loremssd'"
-    :button="'dedede'"
+    :heading="'can i order?'"
+    :subheading="'How to order delivery for a specific day or change the menu'"
+    :button="'view answers'"
   />
   <cardboard-section
-    :heading="'lorem'"
-    :subheading="'loremssd'"
-    :button="'dedede'"
+    :heading="'need catering?'"
+    :subheading="'if you have a little event we can to make catering menu'"
+    :button="'see all'"
     :img="'src/assets/img/cabbage.png'"
     :media="'src/assets/img/color-veg-3.png'"
   />
   <transparent-section
     :class="'photo--2'"
-    :heading="'lorem'"
-    :subheading="'loremssd'"
-    :button="'dedede'"
+    :heading="'be healthy '"
+    :subheading="'we make all for you '"
+    :button="'get in touch!'"
   />
 </template>
 
@@ -45,6 +45,7 @@ import { defineOptions } from "vue";
 defineOptions({
   name: "HomePage",
 });
+
 
 import TransparentSection from "@/components/sections/TransparentSection.vue";
 import CardboardSection from "@/components/sections/CardboardSection.vue";
